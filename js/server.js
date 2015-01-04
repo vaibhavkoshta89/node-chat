e41b25cdeba381e31cabf6d05085cba79a2247d4
vaibhavkoshta89 <vaibhavkoshta89@gmail.com>
2015-01-04 15:16:56 -0500

    Code improvement

--- js/server.js
+++ js/server.js
@@ -6,24 +6,26 @@ var app = express();
 var server = require('http').createServer(app).listen(1337, "localhost");
 var io = require('socket.io')(server);
 
-io.on('connection',function(client){
-	client.on('join',function(data){
+io.on('connection', function(client) {
+	client.on('join', function(data) {
 		client.nickname = data;
-		io.emit('message',client.nickname+ ' Connected..');
+		io.emit('message', client.nickname + ' Connected..');
 	});
-	
-	client.on('sendMessage',function(data){
+
+	client.on('sendMessage', function(data) {
 		console.log(data);
 		var nickname = client.nickname;
-		client.broadcast.emit('message',nickname+": "+data);
-		client.emit('message',nickname+": "+data);
-		
+		client.broadcast.emit('message', nickname + ": " + data);
+		client.emit('message', nickname + ": " + data);
+
+	});
+	client.on('disconnected', function(data) {
+		var nickname = client.nickname;
+		client.emit('message', nickname + " Disconnected");
+
 	});
 });
-	
 
-
-
-app.get('/', function (req, res) {
-res.sendFile(__dirname+'/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
