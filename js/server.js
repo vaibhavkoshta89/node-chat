10b8fc376eeca0d74f2f7534078ad0978f09ac9c
vaibhavkoshta89 <vaibhavkoshta89@gmail.com>
2015-01-03 23:24:57 -0500

    node js test chat app

---
+++ js/server.js
@@ -0,0 +1,29 @@
+/**
+ * New node file
+ */
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app).listen(1337, "localhost");
+var io = require('socket.io')(server);
+
+io.on('connection',function(client){
+	client.on('join',function(data){
+		client.nickname = data;
+		io.emit('message',client.nickname+ ' Connected..');
+	});
+	
+	client.on('sendMessage',function(data){
+		console.log(data);
+		var nickname = client.nickname;
+		client.broadcast.emit('message',nickname+": "+data);
+		client.emit('message',nickname+": "+data);
+		
+	});
+});
+	
+
+
+
+app.get('/', function (req, res) {
+res.sendFile(__dirname+'/index.html');
+});
